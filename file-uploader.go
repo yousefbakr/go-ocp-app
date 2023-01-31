244054f28a23bd7849bd799a921eb0bfbf399dd5
yousefbakr <43080430+yousefbakr@users.noreply.github.com>
2023-01-31 15:16:27 +0200

    Update file-uploader.go

--- file-uploader.go
+++ file-uploader.go
@@ -1,56 +1,33 @@
 package main
 
 import (
-    "context"
-    "log"
-
-    "github.com/minio/minio-go/v7"
-    "github.com/minio/minio-go/v7/pkg/credentials"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
+type App struct {
+	Port string
+}
+
+func (a *App) Start() {
+	addr := fmt.Sprintf(":%s", a.Port)
+	log.Printf("Starting app on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func env(key, defaultValue string) string {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return defaultValue
+	}
+	return val
+}
+
 func main() {
-    ctx := context.Background()
-    endpoint := "play.min.io"
-    accessKeyID := "Q3AM3UQ867SPQQA43P2F"
-    secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
-    useSSL := true
-
-    // Initialize minio client object.
-    minioClient, err := minio.New(endpoint, &minio.Options{
-        Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
-        Secure: useSSL,
-    })
-    if err != nil {
-        log.Fatalln(err)
-    }
-
-    // Make a new bucket called mymusic.
-    bucketName := "mymusic"
-    location := "us-east-1"
-
-    err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
-    if err != nil {
-        // Check to see if we already own this bucket (which happens if you run this twice)
-        exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
-        if errBucketExists == nil && exists {
-            log.Printf("We already own %s\n", bucketName)
-        } else {
-            log.Fatalln(err)
-        }
-    } else {
-        log.Printf("Successfully created %s\n", bucketName)
-    }
-
-    // Upload the zip file
-    objectName := "golden-oldies.zip"
-    filePath := "/tmp/golden-oldies.zip"
-    contentType := "application/zip"
-
-    // Upload the zip file with FPutObject
-    info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
-    if err != nil {
-        log.Fatalln(err)
-    }
-
-    log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+	server := App{
+		Port: env("PORT", "8080"),
+	}
+	server.Start()
 }
