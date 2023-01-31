528ce711007c3bcf50fd8d05e89690729d771712
yousefbakr <43080430+yousefbakr@users.noreply.github.com>
2023-01-31 17:45:20 +0200

    Update file-uploader.go

--- file-uploader.go
+++ file-uploader.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-        "context"
         "log"
 
         "github.com/minio/minio-go/v7"
@@ -9,10 +8,9 @@ import (
 )
 
 func main() {
-        ctx := context.Background()
         endpoint := "http://localhost:9000"
         accessKeyID := "hz9zhbwDXQpxYAjX"
-        secretAccessKey := "3B8lOjyaP0l1JqLXx4bPa65rueJbhpiZ"
+        secretAccessKey := "3B8lOjyaP0l1JqLXx4bPa65rueJbhpiZG"
         useSSL := true
 
         // Initialize minio client object.
@@ -24,33 +22,5 @@ func main() {
                 log.Fatalln(err)
         }
 
-        // Make a new bucket called mymusic.
-        bucketName := "mymusic"
-        location := "us-east-1"
-
-        err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
-        if err != nil {
-                // Check to see if we already own this bucket (which happens if you run this twice)
-                exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
-                if errBucketExists == nil && exists {
-                        log.Printf("We already own %s\n", bucketName)
-                } else {
-                        log.Fatalln(err)
-                }
-        } else {
-                log.Printf("Successfully created %s\n", bucketName)
-        }
-
-        // Upload the zip file
-        objectName := "golden-oldies.zip"
-        filePath := "/tmp/golden-oldies.zip"
-        contentType := "application/zip"
-
-        // Upload the zip file with FPutObject
-        info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
-        if err != nil {
-                log.Fatalln(err)
-        }
-
-        log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+        log.Printf("%#v\n", minioClient) // minioClient is now set up
 }
